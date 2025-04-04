fa7226a448059c0264110f695dff36e58f979402
CSefcek <carlosefcek@gmail.com>
2025-04-05 01:22:49 +0200

    Add Room class

--- main.py
+++ main.py
@@ -1,7 +1,88 @@
+import room
+
 with open('Start.txt') as intro_file:
     read_intro = intro_file.read()
     print(read_intro)
     intro_file.closed
 
 rooms_file = open('Rooms.txt')
-print(rooms_file)
+rooms_file_lines = rooms_file.readlines()
+
+
+
+room_1 = room.Room(False, False, True, True, False, False)
+
+if room_1 == True:
+    print("hello")
+else:
+    print(type(room_1))
+
+
+
+# room = None
+
+# def room_1():
+#     room = True
+#     print(rooms_file_lines[0])
+#     return room
+
+# def room_2():
+#     room = True
+#     print(rooms_file_lines[1])
+#     return room
+
+# def room_3():
+#     room = True
+#     print(rooms_file_lines[2])
+#     return room
+
+# def room_4():
+#     room = True
+#     print(rooms_file_lines[3])
+#     return room
+
+# def room_5():
+#     room = True
+#     print(rooms_file_lines[4])
+#     return room
+
+# def room_6():
+#     room = True
+#     print(rooms_file_lines[5])
+#     return room
+
+# def room_7():
+#     room = True
+#     print(rooms_file_lines[6])
+#     return room
+
+# def room_8():
+#     room = True
+#     print(rooms_file_lines[7])
+#     return room
+
+# def room_9():
+#     room = True
+#     print(rooms_file_lines[8])
+#     return room
+
+# print(room)
+# first_room = room_1()
+# print(first_room)
+# second_room = room_2()
+
+# third_room = room_3()
+
+# fourth_room = room_4()
+
+# fifth_room = room_5()
+
+# sixth_room = room_6()
+
+# seventh_room = room_7()
+
+# eighth_room = room_8()
+
+# nonth_room = room_9()
+
+
---
+++ room.py
@@ -0,0 +1,8 @@
+class Room:
+    def __init__(self, has_exit, room_n, room_s, room_e, room_w, item):
+        self.exit = has_exit
+        self.room_n = room_n
+        self.room_s = room_s
+        self.room_e = room_e
+        self.room_w =room_w
+        self.item = item
